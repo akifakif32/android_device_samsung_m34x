75d58b54354cc0cec1de5e0240b91a3c31dbf653
Ksawlii <maja.blaszczak@proton.me>
2025-08-10 20:41:34 +0200

    m34x: Move tee blobs to a subdirectory
    Change-Id: I57972fd69e15c75578cf3e327c300270ce76856f
    Co-authored-by: Flopster101 <nahuelgomez329@gmail.com>
    Signed-off-by: Ksawlii <maja.blaszczak@proton.me>

--- device.mk
+++ device.mk
@@ -27,5 +27,9 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/audio/audio_board_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_board_info.xml \
     $(DEVICE_PATH)/configs/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
 
+# Init
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/configs/init/init.s5e8825.unify.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.s5e8825.unify.rc
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(DEVICE_PATH)/overlay
