c973607b3f1473bd6b516ad88c86e6d841dfc8aa
Majaahh <maja.blaszczak@proton.me>
2025-09-02 12:17:08 +0200

    m34x: init: Add m34x specific init
    * I compared stock init from common tree (A536B stock vendor) and we were missing this much stuff so let's add it here since it's apparently not needed on every s5e8825 device
    
    Change-Id: I9b0a4d1ab3045daae741402cdd8b5fcfebf7a9d0
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- device.mk
+++ device.mk
@@ -30,6 +30,7 @@ PRODUCT_COPY_FILES += \
 
 # Init
 PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/configs/init/init.m34x.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.m34x.rc \
     $(DEVICE_PATH)/configs/init/init.s5e8825.unify.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.s5e8825.unify.rc
 
 # Nfc
