6219846f0f2c442dee4247082b27ab492f238ab2
Majaahh <maja.blaszczak@proton.me>
2025-09-06 13:29:18 +0200

    m34x: Inherit common products in common tree
    Change-Id: Iaaba84499420b900922ad32102c0c5b5f3d7e8a6
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- lineage_m34x.mk
+++ lineage_m34x.mk
@@ -14,11 +14,6 @@
 # limitations under the License.
 #
 
-## Inherit from generic products, most specific first
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
-
 # Inherit from m34x device
 $(call inherit-product, device/samsung/m34x/device.mk)
 
